4198245cd4a583f027caa52f2e1aee295b61fbcc
Marcel <mail@marcel-blanck.de>
2021-09-27 01:10:12 +0200

    Fix script to work with service

--- run_build_and_install.sh
+++ run_build_and_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cross build -j 6 --target arm-unknown-linux-gnueabihf \
-&& ssh pi@hourglass "killall -9 hourglass" \
+&& ssh pi@hourglass "sudo systemctl stop hourglass.service ; killall -9 hourglass" \
 ; ssh pi@hourglass "mkdir ~/html" \
 ; scp target/arm-unknown-linux-gnueabihf/debug/hourglass pi@hourglass:~/ \
 ; scp html/index.html pi@hourglass:~/html \
-; ssh pi@hourglass "~/hourglass"
+; ssh pi@hourglass "sudo systemctl start hourglass.service"
